8eb9672ecb4a4c9338b650572621d5263e9da058
rolandoquiroz <rolandoquiroz@gmail.com>
2020-07-19 23:37:21 -0500

    Adds 0-lockboxes.py final version

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,58 +3,52 @@
 You have n number of locked boxes in front of you.Each box is numbered
 sequentially from 0 to n - 1 and each box may contain keys to the other
 boxes.
-
 This module contains a method called canUnlockAll that determines
 if all the boxes can be opened.
-
 - Prototype: def canUnlockAll(boxes)
 - boxes is a list of lists
 - A key with the same number as a box opens that box
 - You can assume all keys will be positive integers
 - The first box boxes[0] is unlocked
 - Return True if all boxes can be opened, else return False
-
 This file can also be imported as a module and contains the following
 function methods:
-
     * canUnlockAll - Determines if all the boxes can be opened
 """
 
 
 def canUnlockAll(boxes):
     """Determines if all the boxes can be opened.
-
     Args:
         boxes (list): A list of lists
-
     Returns:
         (bool): True if all boxes can be opened, else return False
     """
     needed_keys = []
-    keys = []
+    got_keys = []
     can_be_opened = []
     opened = []
 
     for i in range(len(boxes)):
         needed_keys.append(i)
-        keys.append(None)
+        got_keys.append(None)
         can_be_opened.append(False)
         opened.append(False)
 
-    keys[0] = 0
+    got_keys[0] = 0
     can_be_opened[0] = True
 
-    while (((can_be_opened) != (opened))):
+    while (can_be_opened != opened):
         for i in range(len(boxes)):
-            if ((keys[i] == i) and (can_be_opened[i] is True) and
+            if ((got_keys[i] == i) and (can_be_opened[i] is True) and
                     (opened[i] is False)):
                 for number in boxes[i]:
-                    if ((number in needed_keys) and (number not in keys)):
-                        keys[number] = number
+                    if ((number in needed_keys) and (number not in got_keys)):
+                        got_keys[number] = number
                         can_be_opened[number] = True
                     else:
                         continue
-                if keys == needed_keys:
+                if got_keys == needed_keys:
                     return True
                 opened[i] = True
     return False
