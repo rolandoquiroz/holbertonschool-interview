7ea26d6951b0fe452635078b70ae96871ce184da
rolandoquiroz <rolandoquiroz@gmail.com>
2020-07-28 00:43:47 +0000

    0-insert_number.c with Betty checked

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -10,30 +10,29 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new = NULL;
-    listint_t **current = head;
+	listint_t *new = NULL;
+	listint_t **current = head;
 
-    new = malloc(sizeof(listint_t));
-    
-    if (new == NULL)
-        return (NULL);
+	new = malloc(sizeof(listint_t));
 
-    new->n = number;
-    new->next = NULL;
+	if (new == NULL)
+		return (NULL);
 
+	new->n = number;
+	new->next = NULL;
 
-    if (*head == NULL)
-    {
-        *head = new;
-        new->next = *head;
-    }
-    else
-    {
-        while (*current != NULL && (*current)->n < new->n)
-            current = &((*current)->next);
-        new->next = *current;
-	    *current = new;
-    }
-
-    return (new);
+	if (*head == NULL)
+	{
+		*head = new;
+		new->next = *head;
+	}
+	else
+	{
+		while (*current != NULL && (*current)->n < new->n)
+			current = &((*current)->next);
+		new->next = *current;
+		*current = new;
+	}
+	return (new);
 }
+
