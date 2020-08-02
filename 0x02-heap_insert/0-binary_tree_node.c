303316ac1d85654ee9017e4bf9ac88facf85545a
rolandoquiroz <rolandoquiroz@gmail.com>
2020-08-02 14:36:55 +0000

    Task 0 file added

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -9,5 +9,16 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new = NULL;
 
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+	new->parent = parent;
+
+	return (new);
 }
