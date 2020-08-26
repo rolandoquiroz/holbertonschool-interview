f2895f5116537266850970b1ff3bba9acacab0c4
rolandoquiroz <rolandoquiroz@gmail.com>
2020-08-26 02:52:10 +0000

    [Fix] Code output: An empty list is considered a palindrome

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -5,9 +5,15 @@
 /**
  * is_palindrome - Checks if a singly linked list is a palindrome
  * @head: pointer to pointer of first node of listint_t list
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
+ *         An empty list is considered a palindrome.
  */
 int is_palindrome(listint_t **head)
 {
+	int ans;
 
+	if (head == NULL)
+		ans = 1;
+
+	return (ans);
 }
