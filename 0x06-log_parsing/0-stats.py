f7914f3dfd26b40272ed06634cecaec6736ae94f
rolandoquiroz <rolandoquiroz@gmail.com>
2020-08-30 18:01:23 -0500

    [Fix] Make file executable

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -8,7 +8,8 @@ readed_status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
 file_size = 0
 lines = 0
 try:
-    for line in sys.stdin:
+    while (True):
+        line = sys.stdin.readline()
         information = list(map(int, (line.partition('1"')[2]).split()))
         if len(information) == 2:
             file_size += information[1]
