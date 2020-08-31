32a03f4f122f985997e2b3ebc99384be48262dcf
rolandoquiroz <rolandoquiroz@gmail.com>
2020-08-30 19:04:33 -0500

    [Fix] getting right info

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,15 +3,15 @@
 import sys
 
 
-readed_status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                       403: 0, 404: 0, 405: 0, 500: 0}
+readed_status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+                       '403': 0, '404': 0, '405': 0, '500': 0}
 file_size = 0
 lines = 0
 try:
     for line in sys.stdin:
-        information = list(map(int, line.split()[-2:]))
+        information = list(line.split(" ")[-2:])
         if len(information) >= 2:
-            file_size += information[1]
+            file_size += int(information[1])
             if information[0] in readed_status_codes.keys():
                 readed_status_codes[information[0]] += 1
         if lines != 0 and lines % 10 == 0:
