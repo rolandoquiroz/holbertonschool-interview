47f14e519c7425e595bf57bb7cd18c338434ba8a
rolandoquiroz <rolandoquiroz@gmail.com>
2020-08-30 20:26:00 -0500

    [Fix] counter logic

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -6,7 +6,7 @@ import sys
 readed_status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
                        '403': 0, '404': 0, '405': 0, '500': 0}
 file_size = 0
-lines = 1
+lines = 0
 try:
     for line in sys.stdin:
         information = list(line.split(" ")[-2:])
@@ -14,7 +14,7 @@ try:
         if len(information) >= 2:
             if information[0] in readed_status_codes.keys():
                 readed_status_codes[information[0]] += 1
-        if lines % 10 == 0:
+        if lines != 0 and lines % 10 == 0:
             print("File size:", file_size)
             for key, value in sorted(readed_status_codes.items()):
                 if value != 0:
