c0b699d5b57e91f84dadb4b8bd40c84a2eda2fb6
rolandoquiroz <rolandoquiroz@gmail.com>
2020-09-30 05:18:15 +0000

    First commit

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,15 @@
+#include "slide_line.h"
+
+/**
+ * slide_line - Slides and merges an array of integers
+ * @line: points to an array of integers containing size elements
+ * that must be slided & merged
+ * @size: size of the array of integers pointed by line
+ * @direction: represents the direction to merge & slide the array
+ * of integers. direction can be either SLIDE_LEFT or SLIDE_RIGHT
+ * Return: 1 upon success, or 0 upon failure
+ */
+
+int slide_line(int *line, size_t size, int direction)
+{
+}
