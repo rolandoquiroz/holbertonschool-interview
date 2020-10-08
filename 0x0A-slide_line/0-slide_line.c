52074fd51ec367bd7f546dfa73b56537663f0154
rolandoquiroz <rolandoquiroz@gmail.com>
2020-10-08 23:45:22 +0000

    raw Solution

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,6 +1,5 @@
 #include "slide_line.h"
 
-
 /**
  * slide_line - Slides and merges an array of integers
  * @line: points to an lineay of integers containing size elements
@@ -10,60 +9,86 @@
  * of integers. direction can be either SLIDE_LEFT or SLIDE_RIGHT
  * Return: 1 upon success, or 0 upon failure
  */
-
 int slide_line(int *line, size_t size, int direction)
 {
-	int my_size, i, j, k, count;
+	int i, j, k, my_size, count = 0;
 
 	if (line == NULL || size < 1 || (direction != 0 && direction != 1))
 	{
 		return (0);
 	}
+
 	my_size = (int) size;
 
-	j = 0;
-	for (i = 0; i < my_size; i++)
+	if (direction == 0)
 	{
-		if (line[i] != 0)
+		/* slide to left */
+		j = 0;
+		for (i = 0; i < my_size; i++)
 		{
-			line[j++] = line[i];
-		}
-	}
-	count = j;
-	for (i = j; i < my_size; i++)
-	{
-		line[i] = 0;
-	}
-	for (i = 0; i < count; i++)
-	{
-		if (line[i] == line[i + 1])
-		{
-			line[i] = 2 * line[i];
-			for (k = i + 1; k < count; k++)
+			if (line[i] != 0)
 			{
-				line[k] = line[k + 1];
+				line[j++] = line[i];
 			}
-			count--;
-			j--;
 		}
+		count = j;
 		for (i = j; i < my_size; i++)
 		{
 			line[i] = 0;
 		}
+		/* merge to left*/
+		j = count;
+		for (i = 0; i < count; i++)
+		{
+			if (line[i] == line[i + 1])
+			{
+				line[i] = line[i] + line[i + 1];
+				/* shifting left next array elements */
+				for (k = i + 1; k < j; k++)
+				{
+					line[k] = line[k + 1];
+				}
+				j--;
+			}
+		}
+		for (k = j; i < my_size; i++)
+		{
+			line[i] = 0;
+		}
 	}
-
 	if (direction == 1)
 	{
+		/* slide to right */
 		j = my_size - 1;
 		for (i = my_size - 1; i > -1; i--)
 		{
 			if (line[i] != 0)
 			{
 				line[j--] = line[i];
+				count++;
 			}
 		}
 		count = j;
 		for (i = j; i > -1; i--)
+		{
+			line[i] = 0;
+		}
+		/* merge to left*/
+		j = count;
+		for (i = my_size - 1; i > count; i--)
+		{
+			if (line[i] == line[i - 1])
+			{
+				line[i] = line[i] + line[i - 1];
+				/* shifting left next array elements */
+				for (k = i - 1; k > j; k--)
+				{
+					line[k] = line[k - 1];
+				}
+				j++;
+			}
+		}
+		for (k = j; i > -1; i--)
 		{
 			line[i] = 0;
 		}
