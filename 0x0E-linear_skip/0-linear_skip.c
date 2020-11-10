9e50778deb2129b84cff00ade5cc0b6f74dd265b
rolandoquiroz <rolandoquiroz@gmail.com>
2020-11-10 20:20:36 +0000

    First commit

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "search.h"
+
+/**
+ * linear_skip - Searches for a value in a sorted skip list of integers.
+ *
+ * @list: Pointer to the head node of the skip list to search in
+ * @value: The value to search for
+ *
+ * Return: Pointer on the first node where value is located
+ *         NULL If value is not present in list or if head is NULL
+ */
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+    if list = NULL;
+
+}
