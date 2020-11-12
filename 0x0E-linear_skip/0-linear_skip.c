e28b03e53ac1219a0d2262c9a9eac6e42ed4238b
rolandoquiroz <rolandoquiroz@gmail.com>
2020-11-12 00:14:06 +0000

    Add Task 0

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -1,19 +1,85 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "search.h"
 
+size_t len(skiplist_t *);
 /**
- * linear_skip - Searches for a value in a sorted skip list of integers.
+ * linear_skip - searches for a value in a sorted skip list of integers
+ * @list: head list pointer
+ * @value: value to search
  *
- * @list: Pointer to the head node of the skip list to search in
- * @value: The value to search for
- *
- * Return: Pointer on the first node where value is located
- *         NULL If value is not present in list or if head is NULL
- */
-
+ * Return: pointer to the value or NULL
+**/
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    if list = NULL;
+	skiplist_t *left = list, *right = NULL;
 
+	if (list == NULL)
+	{
+		return (NULL);
+	}
+
+
+	while (value > left->express->n)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+		       left->express->index, left->express->n);
+		left = left->express;
+		if (left->express == NULL)
+		{
+			break;
+		}
+	}
+
+	if (left->express)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+		       left->express->index, left->express->n);
+		printf("Value found between indexes [%lu] and [%lu]\n",
+		       left->index, left->express->index);
+	}
+	else
+	{
+		printf("Value found between indexes [%lu] and [%lu]\n",
+		       left->index, len(list));
+	}
+	
+	right = left;
+
+	while (value >= right->next->n)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+		       right->index, right->n);
+		right = right->next;
+		if (right->next == NULL || value == right->n)
+		{
+			break;
+		}
+	}
+
+	printf("Value checked at index [%lu] = [%d]\n", right->index, right->n);
+	if (right->next == NULL)
+	{
+		return (NULL);
+	}
+
+	return (right);
+}
+
+/**
+ * len - Calculate length of the list
+ * @list: is a pointer to the head of the skip list to measure in
+ * 
+ * Return: The lenght of the list 
+ */
+size_t count(skiplist_t *list)
+{
+	size_t n = 0;
+
+	while (list->next)
+	{
+		n += 1;
+		list = list->next;
+	}
+	return (n);
 }
