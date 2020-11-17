225db6783c34c7a801054df83dd43cf0fdb0585a
rolandoquiroz <rolandoquiroz@gmail.com>
2020-11-17 17:02:50 -0500

    Add Task 0

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, async function (error1, response1, body) {
+  if (error1) {
+    return console.log(error1);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character in characters) {
+      const response2 = await new Promise((resolve, reject) => {
+        request(characters[character], (error2, response2, html) => {
+          if (error2) {
+            reject(error2);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
+      });
+      console.log(response2);
+    }
+  }
+});
