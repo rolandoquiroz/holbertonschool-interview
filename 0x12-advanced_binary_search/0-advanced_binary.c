8b423f4ac15a1a394f1b1aadd5741208cac42519
rolandoquiroz <rolandoquiroz@gmail.com>
2020-12-07 23:14:19 -0500

    Add Task 0

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -1,15 +1,71 @@
 #include "search_algos.h"
 
+void print_array(int *array, int a, int j);
 /**
- * advanced_binary - searches for a value in a sorted array of integers
- * @array: pointer to the first element of the array to search in
- * @size: number of elements in array
- * @value: the value to search for
- * Return: the index where value is located ||
- *         -1 if value is not present in array or if array is NULL
+ * advanced_binary - finds first occurence of element in array
+ * @array: the sorted array to search
+ * @size: size of array
+ * @value: value to find
+ * Return: index of value or -1
  */
-
 int advanced_binary(int *array, size_t size, int value)
 {
+	int i = 0, j = (int)size - 1, k = 0, aux;
 
+	if (size < 1)
+		return (-1);
+
+	while (i <= j)
+	{
+		k = (((j + i) / 2));
+		printf("Searching in array:");
+		aux = i;
+		print_array(array, aux, j);
+		printf("\n");
+		if (array[k] == value)
+		{
+			if (value == array[k - 1])
+			{
+				j = k;
+			}
+			else
+			{
+				return (k);
+			}
+		}
+		else if (value > array[k])
+		{
+			i = k + 1;
+		}
+		else
+		{
+			if (size % 2)
+			{
+				j = k;
+			}
+			else
+			{
+				j = k - 1;
+			}
+		}
+	}
+	return (-1);
+}
+
+/**
+ * print_array - search value in an array with advanced binary search
+ * @array: array with values to search
+ * @i: minimum position to index
+ * @j: max position
+ * Return: Not return
+ */
+void print_array(int *array, int i, int j)
+{
+	if (i <= j)
+	{
+		printf(" %d", array[i]);
+		if (i < (j))
+			printf(",");
+		print_array(array, i + 1, j);
+	}
 }
