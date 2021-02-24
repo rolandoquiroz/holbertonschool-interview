be870821b9b04388fdd5e119788af4a8fdf955cb
rolandoquiroz <rolandoquiroz@gmail.com>
2021-02-24 13:36:52 -0500

    Add Task 0

--- 0x19-making_change/other.py
+++
@@ -1,9 +0,0 @@
-   minCoins = total
-   if total in coins:
-     return 1
-   else:
-      for i in [c for c in coins if c <= total]:
-         numCoins = 1 + recMC(coins,total-i)
-         if numCoins < minCoins:
-            minCoins = numCoins
-   return minCoins
