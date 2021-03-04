b8f319f326048610f6e06cf295dd193e6b3118a9
rolandoquiroz <rolandoquiroz@gmail.com>
2021-03-03 23:40:25 -0500

    Add Task 0

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -1,6 +1,5 @@
 #include "list.h"
 
-List *add_node(List **list, char *str);
 /**
  * add_node_end - add a node on the end.
  * @list: Double linked list.
@@ -12,7 +11,26 @@ List *add_node_end(List **list, char *str)
 {
 	List *new, *last;
 
-	new = add_node(list, str);
+	if (list == NULL || str == NULL)
+		return (NULL);
+
+	new = (List *)malloc(sizeof(List));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (!new->str)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	if (*list == NULL)
+	{
+		new->next = new->prev = new;
+		*list = new;
+		return (new);
+	}
 
 	last = (*list)->prev;
 
@@ -35,29 +53,6 @@ List *add_node_begin(List **list, char *str)
 {
 	List *new, *last;
 
-	new = add_node(list, str);
-
-	last = (*list)->prev;
-
-	new->next = (*list);
-	new->prev = last;
-	(*list)->prev = last->next = new;
-	*list = new;
-
-	return (new);
-}
-
-/**
- * add_node - add a node.
- * @list: Double linked list.
- * @str: string value in the new node.
- *
- * Return: Memory addres to the new node.
- */
-List *add_node(List **list, char *str)
-{
-	List *new;
-
 	if (list == NULL || str == NULL)
 		return (NULL);
 
@@ -76,7 +71,15 @@ List *add_node(List **list, char *str)
 	{
 		new->next = new->prev = new;
 		*list = new;
+		return (new);
 	}
+
+	last = (*list)->prev;
+
+	new->next = (*list);
+	new->prev = last;
+	(*list)->prev = last->next = new;
+	*list = new;
 
 	return (new);
 }
