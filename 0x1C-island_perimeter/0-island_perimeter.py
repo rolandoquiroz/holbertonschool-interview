e186094bfa8bf8c1ed8d4263595aa20ed2731686
rolandoquiroz <rolandoquiroz@gmail.com>
2021-03-16 16:22:59 -0500

    Add Task 0

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -27,4 +27,15 @@ def island_perimeter(grid):
     perimeter: int
         perimeter of the island described in grid
     """
-    return perimeter
+    land = 0
+    edge = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                land += 1
+                if i > 0 and grid[i-1][j] == 1:
+                    edge += 1
+                if j > 0 and grid[i][j-1] == 1:
+                    edge += 1
+    return land * 4 - edge * 2
